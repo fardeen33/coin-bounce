614584231cd353a73ddd876272e507b427a44731
fardeen33 <shahfardeen23@gmail.com>
2023-07-24 21:48:21 +0530

    port changes

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,6 +1,6 @@
 const dotenv = require('dotenv').config();
 
-const PORT =  process.env.PORT;
+const PORT =  process.env.PORT || 5000;
 
 const MONGODB_CONNECTION_STRING = process.env.MONGODB_CONNECTION_STRING;
 
